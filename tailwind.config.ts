64738ef361aceacdc3ce46e62cdd228846b35e4b
arihanv <arihanvaranasi@gmail.com>
2024-07-09 22:26:31 -0700

    ui: add tailwind animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,10 +58,29 @@ const config: Config = {
 					DEFAULT: "hsl(var(--card))",
 					foreground: "hsl(var(--card-foreground))",
 				},
+				keyframes: {
+					"accordion-down": {
+					  from: { height: "0" },
+					  to: { height: "var(--radix-accordion-content-height)" },
+					},
+					"accordion-up": {
+					  from: { height: "var(--radix-accordion-content-height)" },
+					  to: { height: "0" },
+					},
+					"blink": {
+					  "0%, 100%": { opacity: "1" },
+					  "50%": { opacity: "0" },
+					},
+				  },
+				  animation: {
+					"accordion-down": "accordion-down 0.2s ease-out",
+					"accordion-up": "accordion-up 0.2s ease-out",
+					"blink": "blink 1s infinite",
+				},
 			},
 		},
 	},
 	presets: [require("./rubric.preset.ts")],
-	plugins: [require("@tailwindcss/typography")],
+	plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
 };
 export default config;
